559907e517c3de851c28347a1ec771b246fc030b
Valeriia <valeriia.air@gmail.com>
2021-05-17 19:08:02 +0300

    рендерится разметка списка меню

---
+++ src/js/menuList.js
@@ -0,0 +1,12 @@
+import menu from '../menu.json'
+import menuTemplate from '../templates/menu.hbs'
+
+const menuList = document.querySelector('.js-menu')
+
+const menuData = {
+  menu: menu,
+}
+
+const menuHTML = menuTemplate(menuData)
+
+menuList.innerHTML = menuHTML;
